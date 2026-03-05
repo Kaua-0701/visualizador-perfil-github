128865275edf755aac6aaec91ee9bbfb796d4391
Kauã Ribeiro Mendes <ribeirocyber123@gmail.com>
2026-03-05 18:22:37 -0300

    feat: adiciona foto, nome, biografia do perfil do github

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+
+    const inputSearch = document.getElementById('input-search');
+    const btnSearch = document.getElementById('btn-search');
+    const profileResults = document.querySelector('.profile-results');
+
+    const baseURL = 'https://api.github.com';
+
+    btnSearch.addEventListener('click', async () => {
+        const userName = inputSearch.value;
+        if (userName) {
+
+            profileResults.innerHTML = `<p class = "loading">Carregando...</p>`;
+            
+            try {
+            const response = await fetch(`${baseURL}/users/${userName}`);
+
+            if (!response.ok) {
+                alert("Usuário não encontrado. Por favor, verifique se o nome do usuário é existente e tente novamente.")
+                return;
+            }
+
+            const userData = await response.json();
+            console.log(userData);
+            
+            profileResults.innerHTML = `
+            <div class = "profile-card">
+
+                <img src = "${userData.avatar_url}" alt = "Avatar de ${userData.name}" class = "profile-avatar"> 
+
+                <div class = "profile-info">
+                    <h2>${userData.name}</h2>
+                    <p>${userData.bio || 'Não possui biografia cadastrada 😢.'}</p>
+                </div>
+            </div>
+            `
+
+            } catch (error) {
+                console.error('Erro ao buscar perfil do usuário:', error);
+                alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente novamente mais tarde.');
+            }
+    
+
+        } else {
+            alert('Por favor, digite um nome de usuário do Github.')
+        }
+    });
+      
