de12e926a06f5d795a190148572ed7e9ef2aa561
Kauã Ribeiro Mendes <ribeirocyber123@gmail.com>
2026-03-05 20:34:31 -0300

    feat: adiciona lista com os 10 últimos repositórios

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,8 +1,19 @@
-export async function fetchGithubUser(userName) {
-    const baseURL = 'https://api.github.com';
+ const baseURL = 'https://api.github.com';
+
+export async function fetchGithubUser(userName) {   
     const response = await fetch(`${baseURL}/users/${userName}`);
     if (!response.ok) {
         throw new Error('Usuário não encontrado.');
+    }
+
+    return await response.json();
+}
+
+
+export async function fetchGithubUsersRepos(userName) {
+    const response = await fetch(`${baseURL}/users/${userName}/repos?per_page=10&sort=created`);
+    if (!response.ok) {
+        throw new Error('Repositório não encontrado.');
     }
 
     return await response.json();
